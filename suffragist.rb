334b936d72ff6ecab1f04c787cfb7a9f17fb7cf6
totz <yuta.totz@gmail.com>
2015-03-11 01:50:57 -0700

    Create cast view

--- suffragist.rb
+++ suffragist.rb
@@ -1,7 +1,14 @@
 require 'sinatra'
 
 get '/' do
+  @title = 'Welcome to the Suffragist!'
   erb :index
+end
+
+post '/cast' do
+  @title = 'Thanks for casting your vote!'
+  @vote =  params['vote']
+  erb :cast
 end
 
 Choices = {
